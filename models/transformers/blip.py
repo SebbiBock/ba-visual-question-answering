5460e784ef549c76ad0f1635092209de3cbc6b3a
SebbiBock <sebastian.bock@stud.tu-darmstadt.de>
2023-04-17 15:48:09 +0200

    Process output for BLIP Transformer to receive output token from forward call

--- models/transformers/blip.py
+++ models/transformers/blip.py
@@ -3,16 +3,20 @@
     https://huggingface.co/Salesforce/blip-vqa-base
 """
 
+import torch
+
 from PIL import Image
 from transformers import BlipProcessor, BlipForQuestionAnswering
-from typing import Dict
+from typing import Dict, Optional
 
 
 CONFIG = {
     "MODEL_PATH": "Salesforce/blip-vqa-base",
     "PROCESSOR_PATH":  "Salesforce/blip-vqa-base",
     "PATCH_SIZE": 16,
-    "ATTENTION_LAYER_HOOK_NAME": "self_attn.dropout"
+    "ATTENTION_LAYER_HOOK_NAME": "self_attn.dropout",
+    "": "",
+    "": "",
 }
 
 
@@ -54,3 +58,41 @@ def preprocess(processor, question: str, answer: str, image: Image.Image) -> Dic
     inputs["labels"] = label
 
     return inputs
+
+
+def process_output(model: torch.nn.Module, image_embeds: torch.FloatTensor, input_ids: torch.LongTensor, attention_mask=None) -> torch.LongTensor:
+    """
+        Method to process the output further. Since the forward method of BLIP is usually only used for training,
+        it does not return the output token. To fix this, the additional steps in generation for the BLIP model
+        are executed with given model, input and image_embeds (from the output).
+    """
+
+    with torch.no_grad():
+
+        # Compute image attention mask from image embeds
+        image_attention_mask = torch.ones(image_embeds.size()[:-1], dtype=torch.long)
+
+        # Get question output to get question embedding
+        question_embeds = model.text_encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            encoder_hidden_states=image_embeds,
+            encoder_attention_mask=image_attention_mask,
+            return_dict=False,
+        )[0]
+
+        # Compute question attention mask
+        question_attention_mask = torch.ones(question_embeds.size()[:-1], dtype=torch.long)
+
+        bos_ids = torch.full(
+            (question_embeds.size(0), 1), fill_value=model.decoder_start_token_id
+        )
+
+        # Create proper logits output and return it
+        return model.text_decoder.generate(
+            input_ids=bos_ids,
+            eos_token_id=model.config.text_config.sep_token_id,
+            pad_token_id=model.config.text_config.pad_token_id,
+            encoder_hidden_states=question_embeds,
+            encoder_attention_mask=question_attention_mask
+        )
