5ba64cb11a4a176a29e66b886f3fb69fc6f388c5
SebbiBock <sebastian.bock@stud.tu-darmstadt.de>
2023-05-10 16:35:06 +0200

    Implement function to compute and efficiently store model heatmaps and predictions

--- compute_model_predictions.py
+++ compute_model_predictions.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 import models.transformers.vilt as vilt
@@ -10,8 +9,6 @@ import util.data_util as dutil
 from attention.attention_rollout import attention_rollout
 from attention.grad_cam import compute_grad_cam_for_layers
 from attention.hooks import EncapsulateTransformerAttention, EncapsulateTransformerActivationAndGradients
-from plotting.plot_comparison import plot_overview_for_question
-from util.image import fuze_image_and_array, resize_array_to_img
 
 
 def main():
@@ -45,7 +42,6 @@ def main():
     images_for_plotting = loader.load_images(question_ids)
     questions = loader.load_questions(question_ids, fuze_ids_and_questions=model_package.CONFIG["MODEL_WRAPPER_USED"])
     annotated_answers = loader.load_annotated_answers(question_ids, single_answer=True)
-    human_answers_df = loader.load_human_answers()
 
     # Encapsulate model and choose the proper attention layer for the forward hook registration
     encapsulated_model = EncapsulateTransformerAttention(
@@ -63,9 +59,7 @@ def main():
     # List of heatmaps to be generated
     grad_cam_heatmaps = []
     att_heatmaps = []
-    human_heatmaps = []
     model_answers = []
-    human_answers = []
 
     # For every image-question pair, get gradients and activations
     for image_eval, image_plot, question, q_id in zip(images_for_eval, images_for_plotting, questions, question_ids):
@@ -80,18 +74,7 @@ def main():
         grad_cam_heatmap = compute_grad_cam_for_layers(
             encapsulated_gradient_model.gradients, encapsulated_gradient_model.activations
         )[0]
-
-        # Resize to image and fuze
-        resized_grad_heatmap = resize_array_to_img(image_plot, grad_cam_heatmap)
-        grad_cam_heatmaps.append(fuze_image_and_array(image_plot, resized_grad_heatmap))
-
-        # Get mean human heatmap, resize and fuze
-        human_heatmap = loader.load_human_heatmaps(q_id, reduction=np.mean)
-        resized_human_heatmap = resize_array_to_img(image_plot, human_heatmap)
-        human_heatmaps.append(fuze_image_and_array(image_plot, resized_human_heatmap))
-
-        # Get human answers
-        human_answers.append(dutil.get_answers_for_question(human_answers_df, q_id))
+        grad_cam_heatmaps.append(grad_cam_heatmap)
 
     # Release hooks so that second encapsulator works
     encapsulated_gradient_model.release()
@@ -127,26 +110,16 @@ def main():
             amount_image_patches=amount_image_patches,
             head_fusion="max"
         )
-
-        # Resize heatmap and fuze with image
-        resized_heatmap = resize_array_to_img(image_plot, rollout_attention_map)
-        att_heatmaps.append(fuze_image_and_array(image_plot, resized_heatmap))
+        att_heatmaps.append(rollout_attention_map)
 
     # Release hooks
     encapsulated_model.release()
 
-    # Create plot for every question-image pair to compare heatmaps
-    for idx, q_id in enumerate(question_ids):
-
-        plot_overview_for_question(
-            human_heatmap=human_heatmaps[idx],
-            grad_cam_heatmap=grad_cam_heatmaps[idx],
-            att_rollout_heatmap=att_heatmaps[idx],
-            human_answers=human_answers[idx],
-            model_answer=model_answers[idx],
-            question=questions[idx],
-            question_id=q_id
-        )
+    # Save outputs
+    model_output_paths = dutil.create_model_output_folders(model_package.__name__.split(".")[-1])
+    dutil.save_att_heatmaps(model_output_paths, question_ids, att_heatmaps)
+    dutil.save_grad_heatmaps(model_output_paths, question_ids, grad_cam_heatmaps)
+    dutil.save_predictions(model_output_paths, question_ids, model_answers)
 
 
 if __name__ == '__main__':
